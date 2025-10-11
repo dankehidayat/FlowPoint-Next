60663bd776c3e35f84d0391a8398d354de2d0d56
Danke Hidayat <dnk.hidayat@gmail.com>
2025-10-11 20:15:30 +0700

    eslint ignore during builds

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
-// next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
