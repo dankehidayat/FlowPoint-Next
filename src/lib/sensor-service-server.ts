cf5725ab064baa02c4da151b5cb2fc9718686061
Danke Hidayat <dnk.hidayat@gmail.com>
2026-01-05 16:01:27 +0700

    Fix for prisma (?)

--- src/lib/sensor-service-server.ts
+++ src/lib/sensor-service-server.ts
@@ -3,7 +3,15 @@
 
 import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient();
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
+
+export const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    log: process.env.NODE_ENV === "development" ? ["query"] : [],
+  });
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
 export interface SensorReading {
   id: number;
